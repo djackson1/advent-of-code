105b33f824ad8de0f4060c34b1f0338ebbc7ce40
Daniel Jackson <d95jackson@gmail.com>
2018-12-03 08:57:27 +0000

    Initial commit

---
+++ 2018/solve.js
@@ -0,0 +1,11 @@
+const fs = require('fs')
+const path = require('path')
+
+const [ whichDay, part ] = process.argv.slice(2)
+const dayPadded = whichDay < 10 ? `0${whichDay}` : whichDay
+
+const solutions = require(path.resolve(__dirname, `./lib/day${dayPadded}`))
+
+const input = fs.readFileSync(path.resolve(__dirname, `./inputs/day${dayPadded}.txt`), 'utf-8')
+
+console.log(`Day ${dayPadded} part ${part}: ${solutions[part](input)}`)
