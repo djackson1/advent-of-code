1fa93390973a22c7f277e5cd24fabf47c68e7fcc
Daniel Jackson <d95jackson@gmail.com>
2018-12-25 18:41:57 +0000

    part b

--- tests/day08.spec.js
+++ tests/day08.spec.js
@@ -5,10 +5,17 @@ const DAY_NUMBER = path.basename(__filename).substr(3, 2)
 const solutions = require(path.resolve(__dirname, `../lib/day${DAY_NUMBER}`))
 
 describe(`day ${DAY_NUMBER}`, () => {
+  const input = '2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2'
+
   describe('part a examples', () => {
     it('example 1', () => {
-      const input = '2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2'
       expect(solutions.a(input)).to.equal(138)
+    })
+  })
+
+  describe('part b examples', () => {
+    it('example 1', () => {
+      expect(solutions.b(input)).to.equal(66)
     })
   })
 })
