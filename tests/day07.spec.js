fa618840de5c385ec38e5cdbbef3d61ee42eb132
Daniel Jackson <d95jackson@gmail.com>
2018-12-25 15:08:32 +0000

    day 7 complete...

--- tests/day07.spec.js
+++ tests/day07.spec.js
@@ -5,29 +5,25 @@ const DAY_NUMBER = path.basename(__filename).substr(3, 2)
 const solutions = require(path.resolve(__dirname, `../lib/day${DAY_NUMBER}`))
 
 describe(`day ${DAY_NUMBER}`, () => {
+  const inputs = [
+    'Step C must be finished before step F can begin.',
+    'Step C must be finished before step A can begin.',
+    'Step A must be finished before step B can begin.',
+    'Step A must be finished before step D can begin.',
+    'Step B must be finished before step E can begin.',
+    'Step D must be finished before step E can begin.',
+    'Step F must be finished before step E can begin.',
+  ]
+
   describe('part a examples', () => {
     it('example 1', () => {
-      const inputs = [
-        'Step C must be finished before step F can begin.',
-        'Step C must be finished before step A can begin.',
-        'Step A must be finished before step B can begin.',
-        'Step A must be finished before step D can begin.',
-        'Step B must be finished before step E can begin.',
-        'Step D must be finished before step E can begin.',
-        'Step F must be finished before step E can begin.',
-      ]
       expect(solutions.a(inputs.join('\n'))).to.equal('CABDFE')
     })
   })
 
-  // describe('part b examples', () => {
-  //   it('example 1', () => {
-  //     const inputs = [
-  //       '#1 @ 1,3: 4x4',
-  //       '#2 @ 3,1: 4x4',
-  //       '#3 @ 5,5: 2x2'
-  //     ]
-  //     expect(solutions.b(inputs.join('\n'))).to.equal(3)
-  //   })
-  // })
+  describe('part b examples', () => {
+    it('example 1', () => {
+      expect(solutions.b(inputs.join('\n'), 0, 2)).to.equal(15)
+    })
+  })
 })
